22723e982efa60ad89599c013fb0dfd78a9caac4
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2022-03-29 16:07:10 -0300

    Base Layout

---
+++ components/playerLayout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const PlayerLayout = ({ children }: Props) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
