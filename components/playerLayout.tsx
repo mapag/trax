64905bda39283a8acb8dc8c43ebfce85614e3aea
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2022-03-29 17:08:38 -0300

    Base Sidebar

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +10,7 @@ const PlayerLayout = ({ children }: Props) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
