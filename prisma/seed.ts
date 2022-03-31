847be999c4ac6ce95feb6a7cbb504c895f69c332
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2022-03-30 22:12:03 -0300

    update seed script - add user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { bcrypt } from "bcrypt";
+import bcrypt from "bcrypt";
 import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
@@ -17,6 +17,36 @@ const run = async () => {
               name: song.name,
               duration: song.duration,
               url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+      name: "Mateo",
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map((_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
             })),
           },
         },
